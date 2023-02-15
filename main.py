f19fa43d66279584605185a08f9ae0731c83778e
gery28 <koczan.geri@gmail.com>
2023-02-15 19:19:19 +0100

    version 3

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import pygame
 import random
 
 pygame.init()
-screen_width=900
-screen_height=700
+screen_width=1200
+screen_height=800
 screen=pygame.display.set_mode((screen_width,screen_height))
 clock =pygame.time.Clock()
 cord_x=40
 cord_y=240
-SPEED=7
+SPEED=10
 facing_left=True
 game_over = False
 
@@ -17,17 +17,17 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-    screen.fill((0, 255, 255))
-    if facing_left and cord_x<=760:
+    screen.fill((140, 100, 125))
+    if facing_left and cord_x<=1160:
         cord_x+=SPEED
-    elif facing_left and cord_x>760:
+    elif facing_left and cord_x>1160:
         facing_left=False
-        cord_x=759
+        cord_x=1159
     elif not facing_left and cord_x >=40:
         cord_x-=SPEED
     elif not facing_left and cord_x<40:
         facing_left=True
-    pygame.draw.rect(screen,(255,0,0),(cord_x,cord_y,20,20))
+    pygame.draw.rect(screen,(0,255,0),(cord_x,cord_y,20,20))
     pygame.display.update()
     clock.tick(60)
 
